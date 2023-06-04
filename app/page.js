b94ff7db38fd9145b77ca3a291c06dfe2f78f926
Steven M . Locke <qdb@comcast.net>
2023-06-04 18:12:28 -0400

    off of main

--- app/page.js
+++ app/page.js
@@ -1,10 +1,5 @@
-import { musicBrainzSearch } from "@/lib/mb"
-
 export default async function Home() {
-	const res = await musicBrainzSearch("opiate", "release-group")
-	const data = await res.json()
-
 	return (
-		<pre>{JSON.stringify(data, null, 2)}</pre>
+		<>Things go here</>
 	)
 }
