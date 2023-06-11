3753e518b735e8cadcfdf0b9433866bef7d0bb57
Steven M . Locke <qdb@comcast.net>
2023-06-11 15:03:30 -0400

    Radix UI

--- app/search/page.js
+++ app/search/page.js
@@ -1,3 +1,36 @@
-export default async function Search() {
+"use client"
+import * as Form from '@radix-ui/react-form'
+import { useState } from 'react';
 
+export default function Search() {
+	const [formFields, setFormFields] = useState({ input: "" })
+
+	const changeHandler = (e) => {
+		setFormFields(p => {
+			let copy = { ...p }
+			copy.input = e.target.value
+			return copy
+		})
+	}
+
+	const submitHandler = (e) => {
+		e.preventDefault()
+		alert(JSON.stringify(formFields, null, 2))
+	}
+
+	return (
+		<>
+			<Form.Root onSubmit={submitHandler}>
+				<Form.Field>
+					<Form.Label>Poo</Form.Label>
+					<Form.Control asChild>
+						<input type='text' placeholder='search' onChange={changeHandler} value={formFields.input}></input>
+					</Form.Control>
+				</Form.Field>
+				<Form.Submit>poobutton</Form.Submit>
+			</Form.Root>
+
+			<pre>{JSON.stringify(formFields, null, 2)}</pre>
+		</>
+	)
 }
